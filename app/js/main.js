06a8510b075879748370e4bcefcb896021d17628
Nick <oomsnikita@gmail.com>
2021-01-23 22:15:11 +0100

    slider finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function(){
+  
+  $('.rate-star').rateYo({
+    starWidth: "12px",
+    ratedFill: "#ffa726",
+    readOnly: true,
+    rating: 5
+  });
+
+  $('.product-slider__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false
+  });
+  
+
   var mixer = mixitup('.products__inner-box');
+  
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -22,18 +22,20 @@ gulp.task('style', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
-    'node_modules/magnific-popup/dist/magnific-popup.css'
+    'node_modules/magnific-popup/dist/magnific-popup.css',
+    'node_modules/rateyo/src/jquery.rateyo.css'
   ])
-      .pipe(concat('libs.min.css'))
-      .pipe(cssmin())
+  .pipe(concat('libs.min.css'))
+  .pipe(cssmin())
       .pipe(gulp.dest('app/css'))
-});
-
-gulp.task('script', function(){
-  return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-    'node_modules/mixitup/dist/mixitup.js' /* скачиваем не min файл, потому что сами потом будем все сжимать */
+    });
+    
+    gulp.task('script', function(){
+      return gulp.src([
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/mixitup/dist/mixitup.js'/* скачиваем не min файл, потому что сами потом будем все сжимать */
   ])
       .pipe(concat('libs.min.js'))
       .pipe(uglify())
@@ -60,6 +62,7 @@ gulp.task('browser-sync', function() {
 
 gulp.task('watch', function(){
   gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
+  gulp.watch('app/scss/**/_global.scss', gulp.parallel('sass'))
   gulp.watch('app/*.html', gulp.parallel('html'))
   gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
